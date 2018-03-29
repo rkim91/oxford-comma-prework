e502fa6489d3329c498fa7fe229c6b25e7446876
Richard Kim <richeekim@gmail.com>
2018-03-29 23:58:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,7 @@ def oxford_comma(array)
   if array
     array.join(" and ")
   end
-  # if array.length == 2
-  #
-  #   array.join(" and ")
-  # els
-  if array.length == 1
-    array.join
-  elsif array.length > 2
+  if array.length > 2
     array[-1].prepend "and "
     array.join(", ")
   end
