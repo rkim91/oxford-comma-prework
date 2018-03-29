98f9936a233639a14b64a4d98a0d8ffaf5b331cf
Richard Kim <richeekim@gmail.com>
2018-03-29 23:57:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,11 @@ def oxford_comma(array)
   if array
     array.join(" and ")
   end
-  if array.length == 2
-    # array[-2] << " and "
-    array.join(" and ")
-  elsif array.length == 1
+  # if array.length == 2
+  #
+  #   array.join(" and ")
+  # els
+  if array.length == 1
     array.join
   elsif array.length > 2
     array[-1].prepend "and "
