1a1d122e61f8fb588184757791106d21c487bb81
Richard Kim <richeekim@gmail.com>
2018-03-30 00:04:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,22 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  end
-  if array.length == 2
-    array.join(" and ")
-  end
+  # if array.length == 1
+  #   array.join
+  # end
+  # if array.length == 2
+  #   array.join(" and ")
+  # end
 #   if array.length > 2
 #     array[-1].prepend "and "
 #     array.join(", ")
 #   end
 # end
+if array.length == 2
+ array[-2] << " and "
+ array.join
+ elsif array.length == 1
+   array.join
+ elsif array.length > 2
+   array[-1].prepend "and "
+ array.join(", ")
+ end
+end
